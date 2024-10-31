1f4c0c7ed6491ca53d19720f019165ea12c5f89c
markush97 <markus@hinkel.co>
2024-10-31 22:48:05 +0100

    refactor(backend): Refactor Network

--- backend/src/network/network.service.ts
+++ backend/src/network/network.service.ts
@@ -1,31 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { NetworkInterface } from '../types/network.interface';
 import * as os from 'os';
 
 @Injectable()
 export class NetworkService {
-  async getInterfaces(): Promise<NetworkInterface[]> {
+  async getInterfaces(): Promise<os.NetworkInterfaceInfo[]> {
     const interfaces = os.networkInterfaces();
-    const result: NetworkInterface[] = [];
-
-    for (const [name, addrs] of Object.entries(interfaces)) {
-      if (addrs) {
-        const ipv4 = addrs.find(addr => addr.family === 'IPv4' && !addr.internal);
-        if (ipv4) {
-          result.push({
-            name,
-            info: {
-              address: ipv4.address,
-              netmask: ipv4.netmask,
-              family: 'IPv4',
-              mac: ipv4.mac,
-              internal: ipv4.internal,
-              cidr: ipv4.cidr,
-            },
-          });
-        }
-      }
-    }
+    const result: os.NetworkInterfaceInfo[] = [];
 
     return result;
   }
