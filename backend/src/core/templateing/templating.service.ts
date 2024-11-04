94be2178187fa5bcce93e41d98fa12353131fe5e
markush97 <markus@hinkel.co>
2024-11-04 23:17:38 +0100

    feat(backend): add template module for more generic templating

---
+++ backend/src/core/templateing/templating.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { TemplatingConfigService } from './templating.config.service';
+import { Eta } from "eta"
+import path from 'path';
+
+@Injectable()
+export class CoreTemplatingService implements OnModuleInit {
+    private eta: Eta;
+
+    constructor(private readonly templateConfig: TemplatingConfigService) { }
+
+    onModuleInit() {
+        this.eta = new Eta({ views: path.resolve(__dirname, this.templateConfig.templateFolder) })
+    }
+
+    render(template: string, data: Record<string, unknown>): string {
+        return this.eta.render(template, data)
+    }
+
+}
