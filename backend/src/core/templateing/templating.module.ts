e442f16d552e884305b92f2705dc32adc814261a
markush97 <markus@hinkel.co>
2024-11-04 23:17:38 +0100

    feat(templating): add templating support

---
+++ backend/src/core/templateing/templating.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { TemplatingConfigService } from './templating.config.service';
+import { CoreTemplatingService } from './templating.service';
+
+@Global()
+@Module({
+    providers: [TemplatingConfigService, CoreTemplatingService],
+    exports: [CoreTemplatingService]
+})
+export class CoreTemplatingModule {};
