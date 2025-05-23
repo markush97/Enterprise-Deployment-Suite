f5172b98dc44e77ae657aeeea7793efce3ae9d58
markush97 <markus@hinkel.co>
2025-05-23 14:27:18 +0200

    feat(auth): add endpoint to get own user information

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,35 +2,33 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@n
 import { ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { MTIErrorCodes } from 'src/core/errorhandling/exceptions/mti.error-codes.enum';
 import { JwtAuthGuard } from './strategies/jwt/jwt-auth.guard';
+import { AccountEntity } from './entities/account.entity';
+import { AuthService } from './auth.service';
+import { EntraIdAuthGuard } from './strategies/entraID/entraId.guard';
+import { AccountInfo } from './auth-user.decorator';
+import { AuthTokenPayload } from './strategies/jwt/auth-token.interface';
 
 @Controller('auth')
 export class AuthController {
-    constructor() { }
+    constructor(private readonly authService: AuthService) { }
 
     /**
      * Validate an access-token
-     *
      *
      * @throws {UnauthorizedSavedateException} UnauthorizedSavedateException if the provided token is invalid
      */
     @Post('validate')
     @UseGuards(JwtAuthGuard)
-    @HttpCode(HttpStatus.OK)
-    @ApiOkResponse({
-        description: 'The token is valid',
-    })
-    @ApiUnauthorizedResponse({
-        description: 'The token is invalid',
-        schema: {
-            properties: {
-                message: {
-                    example: 'Provided token is either invalid or expired',
-                },
-                savedateErrorCode: {
-                    example: MTIErrorCodes.JWTInvalid,
-                },
-            },
-        },
-    })
     public async validateToken(): Promise<void> { }
+
+    @Get()
+    public async getAccounts(): Promise<AccountEntity[]> {
+        return this.authService.getAccounts();
+    }
+
+    @Get('self')
+    @UseGuards(JwtAuthGuard)
+    public async getOwnAccount(@AccountInfo() accountInfo: AuthTokenPayload): Promise<AccountEntity> {
+        return this.authService.getOwnAccount(accountInfo.sub)
+    }
 }
