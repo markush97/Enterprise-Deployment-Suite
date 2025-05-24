b89bd2cec6a1cba91b44b58d05464ad8904cf69e
markush97 <markus@hinkel.co>
2025-05-25 00:03:16 +0200

    test(auth): add extra e2e tests

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -6,6 +6,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { setupApp } from '../src/main';
 import {
+  TOKEN_PAYLOAD_VALID,
   getExpiredJwt,
   getInvalidJwt,
   getMalformedJwt,
@@ -16,12 +17,41 @@ import { setupTestConfig } from './testutils/setup.testutil';
 
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
+  let mockAccountRepository: Record<string, jest.Mock>;
+  let mockRefreshTokenService: Record<string, jest.Mock>;
+
   beforeAll(async () => {
     setupTestConfig();
 
+    mockAccountRepository = {
+      findOne: jest.fn(),
+      findOneOrFail: jest.fn(),
+      findAll: jest.fn(),
+      create: jest.fn(),
+      delete: jest.fn(),
+    };
+
+    mockRefreshTokenService = {
+      findOne: jest.fn(),
+      findOneOrFail: jest.fn(),
+      findAll: jest.fn(),
+      create: jest.fn(),
+      delete: jest.fn(),
+    };
+
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider('AccountEntityRepository')
+      .useValue(mockAccountRepository)
+      .overrideProvider('RefreshTokenServiceRepository')
+      .useValue(mockRefreshTokenService)
+      .overrideProvider('EntityManager')
+      .useValue({
+        persistAndFlush: jest.fn(),
+      })
+      .compile();
+
     app = moduleRef.createNestApplication();
     // Set test JWT secret and any other env overrides BEFORE app setup
     await setupApp(app);
@@ -32,53 +62,98 @@ describe('AuthController (e2e)', () => {
     await app.close();
   });
 
-  it('/auth/validate (POST) - no token', async () => {
-    return request(app.getHttpServer()).post('/auth/validate').expect(401);
+  describe('/auth (GET)', () => {
+    testEndpointAuth('/auth', 'GET', () => app);
+
+    it('should return all accounts', async () => {
+      const mockAccounts = [
+        { id: 1, username: 'testuser1', email: 'test@example.com' },
+        { id: 2, username: 'testuser2', email: 'test2@example.com' },
+      ];
+
+      mockAccountRepository.findAll.mockResolvedValue(mockAccounts);
+
+      const token = await getValidJwt();
+
+      await request(app.getHttpServer())
+        .get('/auth')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .expect(res => {
+          expect(res.body).toEqual(mockAccounts);
+        });
+
+      expect(mockAccountRepository.findAll).toHaveBeenCalledTimes(1);
+    });
   });
 
-  it('/auth/validate (POST) - valid JWT', async () => {
-    const token = await getValidJwt();
-    return request(app.getHttpServer())
-      .post('/auth/validate')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
+  describe('/auth/validate (POST)', () => {
+    it('should fail with no token', async () => {
+      return request(app.getHttpServer()).post('/auth/validate').expect(401);
+    });
+
+    it('should succeed with valid JWT', async () => {
+      const token = await getValidJwt();
+      return request(app.getHttpServer())
+        .post('/auth/validate')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
+    });
+
+    it('should fail with invalid JWT', async () => {
+      const token = await getInvalidJwt();
+      return request(app.getHttpServer())
+        .post('/auth/validate')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(401);
+    });
+
+    it('should fail with malformed JWT', async () => {
+      const token = getMalformedJwt();
+      return request(app.getHttpServer())
+        .post('/auth/validate')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(401);
+    });
+
+    it('should fail with expired JWT', async () => {
+      const token = await getExpiredJwt();
+      return request(app.getHttpServer())
+        .post('/auth/validate')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(401);
+    });
   });
 
-  it('/auth/validate (POST) - invalid JWT', async () => {
-    const token = await getInvalidJwt();
-    return request(app.getHttpServer())
-      .post('/auth/validate')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(401);
+  describe('/auth/self (GET)', () => {
+    testEndpointAuth('/auth/self', 'GET', () => app);
+    it('should return own account with valid JWT', async () => {
+      const token = await getValidJwt();
+
+      const mockAccount = {
+        id: '123',
+        username: 'testuser',
+        email: 'test@example.com',
+      };
+
+      mockAccountRepository.findOneOrFail.mockResolvedValue(mockAccount);
+
+      await request(app.getHttpServer())
+        .get('/auth/self')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .expect(mockAccount);
+
+      expect(mockAccountRepository.findOneOrFail).toHaveBeenCalledWith(TOKEN_PAYLOAD_VALID.sub);
+    });
   });
 
-  it('/auth/validate (POST) - malformed JWT', async () => {
-    const token = getMalformedJwt();
-    return request(app.getHttpServer())
-      .post('/auth/validate')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(401);
+  describe('/auth/refresh (POST)', () => {
+    it('should return 404 without refresh-token-cookie', async () => {
+      await request(app.getHttpServer()).post('/auth/refresh').expect(401);
+    });
   });
 
-  it('/auth/validate (POST) - expired JWT', async () => {
-    const token = await getExpiredJwt();
-    return request(app.getHttpServer())
-      .post('/auth/validate')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(401);
-  });
-
-  it('/auth/self (GET) - valid JWT', async () => {
-    const token = await getValidJwt();
-    return request(app.getHttpServer())
-      .get('/auth/self')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
-  });
-
-  testEndpointAuth('/auth', 'GET', () => app);
-  testEndpointAuth('/auth/self', 'GET', () => app);
-  testEndpointAuth('/auth/refresh', 'GET', () => app);
   testEndpointAuth('/auth/refresh', 'DELETE', () => app);
   testEndpointAuth('/auth/refresh/6286bf6e-b515-40ba-8877-0179f26fccd5', 'DELETE', () => app);
 });
--- backend/test/testutils/auth.testutil.ts
+++ backend/test/testutils/auth.testutil.ts
@@ -5,7 +5,7 @@ import { INestApplication } from '@nestjs/common';
 
 import { testConfig } from './config.testutil';
 
-const payload = {
+export const TOKEN_PAYLOAD_VALID = {
   email: 'test@example.com',
   login: 'test@example.com',
   sub: 'f4417424-4470-4bca-90f6-ebaa413e3619',
@@ -18,11 +18,17 @@ const jwtSigningBaseOptions: jwt.SignOptions = {
 };
 
 export async function getValidJwt(): Promise<string> {
-  return jwt.sign(payload, testConfig.JWT_SECRET, { ...jwtSigningBaseOptions, expiresIn: '1h' });
+  return jwt.sign(TOKEN_PAYLOAD_VALID, testConfig.JWT_SECRET, {
+    ...jwtSigningBaseOptions,
+    expiresIn: '1h',
+  });
 }
 
 export async function getInvalidJwt(): Promise<string> {
-  return jwt.sign(payload, 'wrong-secret', { ...jwtSigningBaseOptions, expiresIn: '1h' });
+  return jwt.sign(TOKEN_PAYLOAD_VALID, 'wrong-secret', {
+    ...jwtSigningBaseOptions,
+    expiresIn: '1h',
+  });
 }
 
 export function getMalformedJwt(): string {
@@ -30,7 +36,10 @@ export function getMalformedJwt(): string {
 }
 
 export async function getExpiredJwt(): Promise<string> {
-  return jwt.sign(payload, testConfig.JWT_SECRET, { ...jwtSigningBaseOptions, expiresIn: -3600 });
+  return jwt.sign(TOKEN_PAYLOAD_VALID, testConfig.JWT_SECRET, {
+    ...jwtSigningBaseOptions,
+    expiresIn: -3600,
+  });
 }
 
 export function testEndpointAuth(
@@ -38,39 +47,54 @@ export function testEndpointAuth(
   method: 'GET' | 'POST' | 'PUT' | 'DELETE',
   getApp: () => INestApplication,
 ) {
-  describe(`Auth checks for ${method} ${endpoint}`, () => {
-    it(`${endpoint} (${method}) - no token`, async () => {
-      const reqBase = request(getApp().getHttpServer());
-      let req: request.Test;
+  it(`Should fail auth with no token`, async () => {
+    const reqBase = request(getApp().getHttpServer());
+    let req: request.Test;
 
-      if (method === 'GET') {
-        req = reqBase.get(endpoint);
-      } else if (method === 'POST') {
-        req = reqBase.post(endpoint);
-      } else if (method === 'PUT') {
-        req = reqBase.put(endpoint);
-      } else if (method === 'DELETE') {
-        req = reqBase.delete(endpoint);
-      }
+    if (method === 'GET') {
+      req = reqBase.get(endpoint);
+    } else if (method === 'POST') {
+      req = reqBase.post(endpoint);
+    } else if (method === 'PUT') {
+      req = reqBase.put(endpoint);
+    } else if (method === 'DELETE') {
+      req = reqBase.delete(endpoint);
+    }
 
-      return req.expect(401);
-    });
+    return req.expect(401);
+  });
 
-    it(`${endpoint} (${method}) - invalid JWT`, async () => {
-      const token = await getInvalidJwt();
-      const reqBase = request(getApp().getHttpServer());
-      let req: request.Test;
+  it(`Should fail auth with invalid JWT`, async () => {
+    const token = await getInvalidJwt();
+    const reqBase = request(getApp().getHttpServer());
+    let req: request.Test;
 
-      if (method === 'GET') {
-        req = reqBase.get(endpoint);
-      } else if (method === 'POST') {
-        req = reqBase.post(endpoint);
-      } else if (method === 'PUT') {
-        req = reqBase.put(endpoint);
-      } else if (method === 'DELETE') {
-        req = reqBase.delete(endpoint);
-      }
-      return req.set('Authorization', `Bearer ${token}`).expect(401);
-    });
+    if (method === 'GET') {
+      req = reqBase.get(endpoint);
+    } else if (method === 'POST') {
+      req = reqBase.post(endpoint);
+    } else if (method === 'PUT') {
+      req = reqBase.put(endpoint);
+    } else if (method === 'DELETE') {
+      req = reqBase.delete(endpoint);
+    }
+    return req.set('Authorization', `Bearer ${token}`).expect(401);
+  });
+
+  it(`Should fail auth with expired JWT`, async () => {
+    const token = await getExpiredJwt();
+    const reqBase = request(getApp().getHttpServer());
+    let req: request.Test;
+
+    if (method === 'GET') {
+      req = reqBase.get(endpoint);
+    } else if (method === 'POST') {
+      req = reqBase.post(endpoint);
+    } else if (method === 'PUT') {
+      req = reqBase.put(endpoint);
+    } else if (method === 'DELETE') {
+      req = reqBase.delete(endpoint);
+    }
+    return req.set('Authorization', `Bearer ${token}`).expect(401);
   });
 }
